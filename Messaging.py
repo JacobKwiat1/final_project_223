1e77e3ebebf5f0bac5e8da2f83cbfe01efe34eb7
Ryan Kingwill <140992585+rrkingwill@users.noreply.github.com>
2026-05-12 20:42:37 -0700

    Fix duplicate message bug; Change to use os

--- Messaging.py
+++ Messaging.py
@@ -2,12 +2,14 @@
 # create instance of DM_Session ( example: dm = DM_Session(sender_username, receiver_username) )
 # How to send messages:
 # dm.send(message)
+# How to print dm log:
+# dm.print_msgs()
 
 
 import logging
 import datetime
 import json
-from pathlib import Path
+import os
     
 class ChatJSONFormatter(logging.Formatter):
     def __init__(self):
@@ -28,12 +30,15 @@ class ChatJSONFormatter(logging.Formatter):
         return json.dumps(entry)
 
 class DM_Session:
+    chat_logger = None
+
     def __init__(self, sender, receiver):
         self.sender = sender
         self.receiver = receiver
-        self.filename = f"{sender}-{receiver}.txt" if sender < receiver else f"{receiver}?{sender}.txt"
+        self.filename = f"{sender}-{receiver}.txt" if sender < receiver else f"{receiver}-{sender}.txt"
 
-        self.chat_logger = self.chat_logs_setup()
+        if f"ChatLogs_{self.filename}" not in logging.root.manager.loggerDict.keys():
+            DM_Session.chat_logger = self.chat_logs_setup()
         
     def chat_logs_setup(self, level = logging.INFO):
         # Assign ChatJsonFormatter class object to formatter
@@ -55,28 +60,25 @@ class DM_Session:
         
     def get_path(self):
         # Set directory path
-        base_dir = Path(__file__).parent.resolve() / "Logs"
+        folder = "Logs"
 
-        # Make directory if it doesn't exit
-        base_dir.mkdir(exist_ok = True)
+        # Make folder if it does not exist
+        os.makedirs(folder, exist_ok=True)
 
         # Resolve the path
-        file_path = (base_dir / self.filename).resolve()
+        file_path = os.path.join(folder, self.filename)
 
-        # Path sanitizer for traversal check
-        if not file_path.is_relative_to(base_dir):
-            raise ValueError("Path traversal detected.")
-        else:
-            return str(file_path)
+        return file_path
         
     def send(self, msg):
-        self.chat_logger.info({"sender": self.sender, "receiver": self.receiver, "message": msg})
+        DM_Session.chat_logger.info({"sender": self.sender, "receiver": self.receiver, "message": msg})
 
     def print_msgs(self):
         msg = {}
+
+        print(f"\n---Messages with {self.receiver}---")
         with open(self.get_path(), "r") as f:
             for line in f:
                 msg = json.loads(line)
                 print(f"{msg["sender"]}: {msg["message"]}\n   {msg["timestamp"]}")
-
 
